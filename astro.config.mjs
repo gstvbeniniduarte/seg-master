5a4d27264131b9f5ad786cdf216fbda5b3eabe71
gstvbeniniduarte <beninigustavo309@gmail.com>
2024-10-29 12:55:11 -0400

    1.4.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import playformCompress from '@playform/compress';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   output: 'hybrid',
