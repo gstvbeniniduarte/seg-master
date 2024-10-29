d019888e88bdf8946f69f9a56217b227c9e30f12
gstvbeniniduarte <beninigustavo309@gmail.com>
2024-10-29 12:01:03 -0400

    1.4.4

---
+++ src/layouts/google-analytics.ts
@@ -0,0 +1,8 @@
+export function initGoogleAnalytics() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: any[]) {
+    window.dataLayer.push(args);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-2PK46XYXHP');
+}
